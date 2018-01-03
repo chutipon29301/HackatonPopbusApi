cbf93f79c8d2c152fa9404fa4d1a3eb10ed19240
sunboyy <sura_sun@hotmail.com>
2018-01-03 20:57:35 +0700

    Create MORE request token

--- token-manager.js
+++ token-manager.js
@@ -1,45 +1,124 @@
 "use strict";
 const hashFunc = require('sha256');
 function hashToBucket(key) {
-    return parseInt(hashFunc(key), 16) % 7;
+    return parseInt(hashFunc(key), 16) % 23;
 }
 let rawTokens = [
     {
-        name: "Admin",
-        public: "1xnh225k",
-        private: "Yq7ZiAoOEmdYgRNyxUBCbC7B9vcwPslN"
+        name: 'Admin',
+        public: 'vmp66yuu',
+        private: 'o5NMNrwHDgjvsAo3lQ8EG9DCR0Spr8Iy'
     },
     {
-        name: "Group 1",
-        public: "aazkly5h",
-        private: "OQcDA1GRA9dItg5cWF23DAu8nGq0hJLq"
+        name: 'G.1',
+        public: 'cmhekzs6',
+        private: 'wtVFl2TZTIuuRD6jNQK5LO3DxL0AzG9z'
     },
     {
-        name: "Group 2",
-        public: "ypi9v8rq",
-        private: "Ra6y6Dyuhp0K7HmTh6wOAttg6LEu0y0F"
+        name: 'G.2',
+        public: 'n4rzhhcf',
+        private: 'lyW1Lj6FwhOmYKCq5BAk9Cm1HltmzbUr'
     },
     {
-        name: "Group 3",
-        public: "hmv46pwd",
-        private: "xv1K8UcgEUITR5NHsvOPWUf5vATt8cne"
+        name: 'G.3',
+        public: 'gk3jlyke',
+        private: 'xCJx4VbZApwITdURn1jDqyDMFq57xLao'
     },
     {
-        name: "Group 4",
-        public: "3xa7197k",
-        private: "mykam6JKc8L6KJEtuaZ9KSbz4N0be8FY"
+        name: 'G.4',
+        public: 'vo5tmpj7',
+        private: '4Yq2Iffz0ri1f5hjgNYVqhwQ0er71yUy'
     },
     {
-        name: "Group 5",
-        public: "3e931inw",
-        private: "6V4ZgkAGtWfUgJb2kDJynB4fWzihuhIm"
+        name: 'G.5',
+        public: '0p00a5u2',
+        private: 'bR2yejB5NQEFsI0KB3GKV1tpomWOFlF0'
     },
     {
-        name: "Group 6",
-        public: "0olpkinu",
-        private: "jXQ9x9p8PoC3TscQyQL4Na8NwqYxhKGw"
-    }
-];
+        name: 'G.6',
+        public: 'hmwh0rjw',
+        private: 'Fr02yCWC8DugZGjibbUpVXI9FOFErYjm'
+    },
+    {
+        name: 'G.7',
+        public: 'gygey5nx',
+        private: 'NazNRF8ecuUStYnppZTT28ND8PG5yjsl'
+    },
+    {
+        name: 'G.8',
+        public: 'q5j4s7uz',
+        private: 'jfxlON9GqfQmvxxQkork7O4iI7wTiuCf'
+    },
+    {
+        name: 'G.9',
+        public: 'i6bshw4s',
+        private: '76Xr2Bey3PE1qRwU7VMPLtEV9GtBZMQk'
+    },
+    {
+        name: 'G.10',
+        public: 'oq41lqh1',
+        private: 'yl0dKSao5xMVA3QUQ6W0nyRjYj8v8nHu'
+    },
+    {
+        name: 'G.11',
+        public: 'cpexq5b7',
+        private: 'clTnranFyhREPvxfsvAu1Hec0hZxQ6gl'
+    },
+    {
+        name: 'G.12',
+        public: 'l9o7pk84',
+        private: 'nc6oc1A2GJoOVmgPwSVzObPHT11rbKnP'
+    },
+    {
+        name: 'G.13',
+        public: 'z5nhd0qk',
+        private: 'TUhJlC84nB5UfPpSmF6rh7oej990To0s'
+    },
+    {
+        name: 'G.14',
+        public: 'eok8rjoq',
+        private: 'MYJNKnDDQWDUGsDF7RQRYxV7GfKmw1vN'
+    },
+    {
+        name: 'G.15',
+        public: 'n2tr4rtn',
+        private: 'YvvHvOuSGBPNfxxOLe2EsVbfkpXrcRYg'
+    },
+    {
+        name: 'G.16',
+        public: 'f8853vt8',
+        private: 'BsYUg7R7btF4jC2FqX74MXuVmepKaMJD'
+    },
+    {
+        name: 'G.17',
+        public: 'ubz4i6fu',
+        private: 'bJ8C1fF4Js0nMKpf7NDVTxAUZXuDSezW'
+    },
+    {
+        name: 'G.18',
+        public: 'px7wguho',
+        private: 'tlf5DOacp1sUJeDxyohkAzJ69MTqPC9d'
+    },
+    {
+        name: 'G.19',
+        public: '2tz5bkt0',
+        private: 'xvWXg4K4mSxExWRu9buWVA53DmMcobpz'
+    },
+    {
+        name: 'G.20',
+        public: 'lnp93b4u',
+        private: 'gzUuY3OVO3j8QYyq2qcbSEbyJEDh9A9t'
+    },
+    {
+        name: 'G.21',
+        public: 'rzrfrlfb',
+        private: 'Y2XnzUqa63RMT2oft4gkZsDPI4RaT5hL'
+    },
+    {
+        name: 'G.22',
+        public: 'by2bxcmt',
+        private: 'JnLDG7MQ1zYzo9XgJqY5DOEOok4rvf3o'
+    }];
 let tokens = new Array(rawTokens.length);
 rawTokens.forEach(token => {
     let bucket = hashToBucket(token.public);
---
+++ utilities/tokengen.js
@@ -0,0 +1,38 @@
+let hashFunc = require('sha256');
+let tokens = new Array(23);
+let count = 23;
+function hashToBucket(key) {
+    return parseInt(hashFunc(key), 16) % 23;
+}
+function randPublic() {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+    let out = "";
+    for (let i = 0; i < 8; i++) {
+        let random = Math.floor(Math.random() * chars.length);
+        out += chars[random];
+    }
+    return out;
+}
+function randPrivate() {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let out = "";
+    for (let i = 0; i < 32; i++) {
+        let random = Math.floor(Math.random() * chars.length);
+        out += chars[random];
+    }
+    return out;
+}
+while (count > 0) {
+    let public = randPublic();
+    let hash = hashToBucket(public);
+    if (!tokens[hash]) {
+        let private = randPrivate();
+        tokens[hash] = {
+            name: "",
+            public: public,
+            private: private
+        };
+        count--;
+    }
+}
+console.log(tokens);
